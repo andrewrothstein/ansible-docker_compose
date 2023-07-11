7c59a8bf8f759ada6efa6707c883c9ba92a5015c
Andrew Rothstein <andrew.rothstein+github@gmail.com>
2023-07-11 15:29:34 -0400

    migrate from ansible-docker-compose

---
+++ README.md
@@ -0,0 +1,39 @@
+andrewrothstein.docker_compose
+===========================
+![Build Status](https://github.com/andrewrothstein/ansible-docker_compose/actions/workflows/build.yml/badge.svg)
+
+Installs [docker-compose](https://github.com/docker/compose).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.docker_compose
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksum.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/docker/compose/releases/download
+
+# https://github.com/docker/compose/releases/download/v2.10.0/checksums.txt
+
+dl()
+{
+    local lchecksums=$1
+    local ver=$2
+    local os=$3
+    local arch=$4
+    local dotexe=${5:-}
+    local platform="${os}-${arch}"
+    local exe=docker-compose-${platform}${dotexe}
+    # https://github.com/docker/compose/releases/download/v2.10.0/docker-compose-linux-x86_64
+    local url=$MIRROR/v$ver/$exe
+
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(fgrep $exe $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    ver=$1
+    printf "  '%s':\n" $ver
+    local lchecksums="${DIR}/docker-compose-${ver}-checksums.txt"
+    if [ ! -e $lchecksums ];
+    then
+        curl -sSLf -o $lchecksums "${MIRROR}/v${ver}/checksums.txt"
+    fi
+
+    dl $lchecksums $ver darwin aarch64
+    dl $lchecksums $ver darwin x86_64
+    dl $lchecksums $ver linux aarch64
+    dl $lchecksums $ver linux armv6
+    dl $lchecksums $ver linux armv7
+    dl $lchecksums $ver linux ppc64le
+    dl $lchecksums $ver linux riscv64
+    dl $lchecksums $ver linux s390x
+    dl $lchecksums $ver linux x86_64
+    dl $lchecksums $ver windows aarch64 .exe
+    dl $lchecksums $ver windows x86_64 .exe
+}
+
+dl_ver ${1:-2.20.0}
